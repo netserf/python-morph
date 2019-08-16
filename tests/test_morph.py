14e0d5291d615333d949efeb2af273cad2624ece
netserf <greg.horie@telus.com>
2019-08-15 19:23:56 -0700

    lint fixes

--- tests/test_morph.py
+++ tests/test_morph.py
@@ -1,12 +1,15 @@
-import pytest
-from unittest.mock import patch
-from unittest.mock import Mock, MagicMock
-import os
+'''
+Test suite for the morph command line tool.
+'''
+
 from pathlib import Path
+import os
+from unittest.mock import patch
 
 import python_morph.morph as morph
 
 def test_get_config_filename_from_env():
+    '''test that config file can be set with environment variable CONFIG_FILE'''
     # setup environment variable
     configfile = '/my/test/dir/myconfig.yaml'
     os.environ["CONFIG_FILE"] = configfile
@@ -18,6 +21,7 @@ def test_get_config_filename_from_env():
 
 
 def test_get_config_filename_from_home():
+    '''test that config file can be set in home directory'''
     with patch.object(Path, 'home', return_value='/home/user') as mock_home:
         with patch.object(Path, 'is_file', return_value=True) as mock_is_file:
             config = morph.get_config_filename()
@@ -26,5 +30,6 @@ def test_get_config_filename_from_home():
             assert config == '/home/user/.morph.yaml'
 
 def test_get_config_from_global():
+    '''test that config file has its global location set'''
     config = morph.get_config_filename()
     assert config == '/usr/local/etc/morph.yaml'
