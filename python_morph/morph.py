2fbe97605e8297d64b6c50815d08ffda28714466
netserf <greg.horie@telus.com>
2019-08-15 18:41:48 -0700

    add support for home directory config detection

--- python_morph/morph.py
+++ python_morph/morph.py
@@ -1,5 +1,20 @@
+from pathlib import Path
 import os
 
-def get_config_file():
-    configfile = os.getenv('CONFIG_FILE')
-    return configfile
+
+def get_config_filename():
+    ''' config file order of precedence is:
+    1. environment variable CONFIG_FILE
+    2. $HOME/.morph.yaml
+    3. /usr/local/etc/morph.yaml
+    '''
+    envfile = os.getenv('CONFIG_FILE')
+    homefile = Path(str(Path.home()) + "/.morph.yaml")
+    globalfile = '/usr/local/etc/morph.yaml'
+    configfile = ''
+    if envfile:
+        return envfile
+    elif homefile.is_file():
+        return str(homefile)
+    else:
+        return globalfile
--- tests/test_morph.py
+++ tests/test_morph.py
@@ -1,11 +1,27 @@
 import pytest
+from unittest.mock import patch
+from unittest.mock import Mock, MagicMock
 import os
+from pathlib import Path
+
 import python_morph.morph as morph
 
-def test_set_config_file_check_env():
-    # setup environment
-    config_name = '/my/test/dir/myconfig.yaml'
-    os.environ["CONFIG_FILE"] = config_name
-    env_config = morph.get_config_file()
-    assert env_config == config_name
+def test_get_config_filename_from_env():
+    # setup environment variable
+    configfile = '/my/test/dir/myconfig.yaml'
+    os.environ["CONFIG_FILE"] = configfile
+    # check configfile can be configured through the environment variable
+    envfile = morph.get_config_filename()
+    assert envfile == configfile
+    # clean up environment variable
+    del os.environ['CONFIG_FILE']
+
+
+def test_get_config_filename_home_called():
+    with patch.object(Path, 'home', return_value='/home/user') as mock_home:
+        with patch.object(Path, 'is_file', return_value=True) as mock_is_file:
+            config = morph.get_config_filename()
+            mock_home.assert_called()
+            mock_is_file.assert_called()
+            assert config == '/home/user/.morph.yaml'
 
